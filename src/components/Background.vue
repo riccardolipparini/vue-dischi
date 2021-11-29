ed311deae7c91ba7acb9f8908df72a402efa6ef1
Riccardo <riccardolipparini0@gmail.com>
2021-11-29 12:49:24 +0100

    Debug

---
+++ src/components/Background.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div id="spotify">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Spotify_App_Logo.svg/2048px-Spotify_App_Logo.svg.png"
+        alt=""
+      />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Background",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+#spotify {
+  display: flex;
+  align-items: center;
+  height: 100px;
+  width: 100%;
+  background-color: grey;
+  img {
+    width: 80px;
+    height: 80px;
+  }
+}
+
+</style>
---
+++ src/components/Selezione.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="container-big">
+      <div class="containcards">
+          <div class="cards">
+              <img src="" alt="">
+              <h1></h1>
+              <h4></h4>
+              <h4></h4>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Selezione",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+#container-big {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: rgb(42, 43, 66);
+  .containcards{
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      height: 700px;
+      width: 80%;
+      .cards{
+          text-align: center;
+          height: 40%;
+          width: calc(90% / 5);
+          background-color: rgb(36, 37, 47);
+          img{
+              width: 150px;
+          }
+          h1{
+              color: white;
+          }
+          h4{
+              color: lightgray;
+          }
+      }
+  }
+}
+</style>
