cb5b261086538fc20c383d380cf4329fa82edeee
Riccardo <riccardolipparini0@gmail.com>
2021-11-29 15:40:08 +0100

    prima parte

---
+++ src/components/Cards.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cards">
+    <img :src="details.poster" alt="" />
+    <h1>{{details.title}}</h1>
+    <h4>{{details.author}}</h4>
+    <h4>{{details.year}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cards",
+  props: {
+      details: Object
+  }
+
+};
+</script>
+
+<style lang="scss">
+.cards {
+  text-align: center;
+  height: 40%;
+  width: calc(90% / 5);
+  background-color: rgb(36, 37, 47);
+  img {
+    width: 150px;
+  }
+  h1 {
+    color: white;
+  }
+  h4 {
+    color: lightgray;
+  }
+}
+</style>
--- src/components/Selezione.vue
+++ src/components/Selezione.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="container-big">
-      <div class="containcards">
-          <div class="cards">
-              <img src="" alt="">
-              <h1></h1>
-              <h4></h4>
-              <h4></h4>
-          </div>
-      </div>
+    <div class="containcards">
+      <Cards v-for="(song, i) in canzoni" :key="i" :details="song" />
+    </div>
   </div>
 </template>
 
+
+
 <script>
+import axios from "axios";
+import Cards from "@/components/Cards.vue";
+
 export default {
   name: "Selezione",
-  components: {},
+  components: {
+    Cards,
+  },
+  data() {
+    return {
+      apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+      canzoni: [],
+    };
+  },
+  created() {},
+  methods: {
+    getCards() {
+      axios.get(this.apiUrl).then((result) => {
+        this.canzoni = result.data;
+      });
+    },
+  },
 };
 </script>
 
@@ -25,27 +41,27 @@ export default {
   align-items: center;
   height: 100vh;
   background-color: rgb(42, 43, 66);
-  .containcards{
-      display: flex;
-      justify-content: space-between;
-      flex-wrap: wrap;
-      height: 700px;
-      width: 80%;
-      .cards{
-          text-align: center;
-          height: 40%;
-          width: calc(90% / 5);
-          background-color: rgb(36, 37, 47);
-          img{
-              width: 150px;
-          }
-          h1{
-              color: white;
-          }
-          h4{
-              color: lightgray;
-          }
+  .containcards {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    height: 700px;
+    width: 80%;
+    .cards {
+      text-align: center;
+      height: 40%;
+      width: calc(90% / 5);
+      background-color: rgb(36, 37, 47);
+      img {
+        width: 150px;
       }
+      h1 {
+        color: white;
+      }
+      h4 {
+        color: lightgray;
+      }
+    }
   }
 }
 </style>
